e9899f1272b6912221a56fcc09b81c4b2f55c7d0
LoicUV <huve.loic@gmail.com>
2013-10-06 22:52:34 +0200

    front FAQ

--- themes/lcdb_theme/js/faq.js
+++ themes/lcdb_theme/js/faq.js
@@ -4,18 +4,17 @@ $(function ($) {
 	$(window).load(function() {
 	
 		/*clic sur un lien"*/
-		$(".liste_FAQ li a").click(function(e){
+		$(".liste_FAQ li a").click(function(e) {
 			e.preventDefault();
-			if(!$(this).parent().hasClass("FAQ_open"))
-			{
-				$(".bloc_"+$(this).parent().attr('id')).css("display", "block");
-				$(this).parent().addClass("FAQ_open");
+			var $this = $(this);
+			if(!$this.parent().hasClass("FAQ_open")) {
+				$this.parent().find(".content").css("display", "block");
+				$this.parent().addClass("FAQ_open");
+			} else {
+				$this.parent().find(".content").css("display", "none");
+				$this.parent().removeClass("FAQ_open");
 			}
-			else
-			{
-				$(".bloc_"+$(this).parent().attr('id')).css("display", "none");
-				$(this).parent().removeClass("FAQ_open");
-			}
+			return false;
 		});
 		
 	});
