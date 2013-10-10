6c01645d323c230932b6ce7a582892fd3f0895c6
plberret <pl.berret@gmail.com>
2013-10-10 10:20:52 +0200

    delivery

--- override/classes/Address.php
+++ override/classes/Address.php
@@ -2,7 +2,7 @@
 
 class Address extends AddressCore
 {
-	
+
 	/**
 	 * Get zone id for a given address
 	 *
@@ -17,7 +17,9 @@ class Address extends AddressCore
 		FROM `'._DB_PREFIX_.'address` a
 		WHERE a.`id_address` = '.(int)$id_address);
 
-		return $this->getZoneByZip();
+		$_idZones[$id_address] = self::getZoneByZipCode($cp);
+
+		return self::$_idZones[$id_address];
 	}
 
 	/**
@@ -26,21 +28,20 @@ class Address extends AddressCore
 	 * @param string $zipcode
 	 * @return integer Zone id
 	 */
-	public static function getZoneByZip($cp)
+	public static function getZoneByZipCode($cp)
 	{
 
 		if (substr($cp, 0, 2) == "75") {
-			self::$_idZones[$id_address] = 1; // Paris
+			$idZone = 1; // Paris
 		} else if (ZoneCustom::isProche($cp)){
-			self::$_idZones[$id_address] = 9; // Proche banlieue
+			$idZone = 9; // Proche banlieue
 		} else if (ZoneCustom::isGrande($cp)) {
-			self::$_idZones[$id_address] = 10; // Grande banlieue
+			$idZone = 10; // Grande banlieue
 		} else {
-			self::$_idZones[$id_address] = 11; // Province
+			$idZone = 11; // Province
 		}
 
-		return self::$_idZones[$id_address];
+		return $idZone;
 
 	}
 }
-
