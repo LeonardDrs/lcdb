1a87d9ead7c5cea7ea3c23fc6db958259b2a88b1
pl <pl.berret@gmail.com>
2014-01-20 11:42:38 +0100

    new field carrier

--- override/classes/Carrier.php
+++ override/classes/Carrier.php
@@ -8,38 +8,45 @@ class Carrier extends CarrierCore
 	/**
 	 * @see ObjectModel::$definition
 	 */
-	public static $definition = array(
-		'table' => 'carrier',
-		'primary' => 'id_carrier',
-		'multilang' => true,
-		'multilang_shop' => true,
-		'fields' => array(
-			/* Classic fields */
-			'id_reference' => 			array('type' => self::TYPE_INT),
-			'name' => 					array('type' => self::TYPE_STRING, 'validate' => 'isCarrierName', 'required' => true, 'size' => 64),
-			'active' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool', 'required' => true),
-			'is_free' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'url' => 					array('type' => self::TYPE_STRING, 'validate' => 'isAbsoluteUrl'),
-			'shipping_handling' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'shipping_external' => 		array('type' => self::TYPE_BOOL),
-			'range_behavior' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'shipping_method' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-			'max_width' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-			'max_height' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-			'max_depth' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-			'max_weight' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
-			'grade' => 					array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'size' => 1),
-			'external_module_name' => 	array('type' => self::TYPE_STRING, 'size' => 64),
-			'is_module' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'need_range' => 			array('type' => self::TYPE_BOOL),
-			'position' => 				array('type' => self::TYPE_INT),
-			'deleted' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+    public static $definition = array(
+        'table' => 'carrier',
+        'primary' => 'id_carrier',
+        'multilang' => true,
+        'multilang_shop' => true,
+        'fields' => array(
+            /* Classic fields */
+            'id_reference' => 			array('type' => self::TYPE_INT),
+            'name' => 					array('type' => self::TYPE_STRING, 'validate' => 'isCarrierName', 'required' => true, 'size' => 64),
+            'type_carrier' => 			array('type' => self::TYPE_BOOL, 'validate' => 'isUnsignedInt'),
+            'active' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool', 'required' => true),
+            'is_free' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'url' => 					array('type' => self::TYPE_STRING, 'validate' => 'isAbsoluteUrl'),
+            'shipping_handling' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'shipping_external' => 		array('type' => self::TYPE_BOOL),
+            'range_behavior' => 		array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'shipping_method' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+            'max_width' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+            'max_height' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+            'max_depth' => 				array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+            'max_weight' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+            'grade' => 					array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'size' => 1),
+            'external_module_name' => 	array('type' => self::TYPE_STRING, 'size' => 64),
+            'is_module' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'need_range' => 			array('type' => self::TYPE_BOOL),
+            'position' => 				array('type' => self::TYPE_INT),
+            'deleted' => 				array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'lon' => 			        array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 100),
+            'lat' => 			        array('type' => self::TYPE_STRING, 'validate' => 'isString', 'size' => 100),
 
-			/* Lang fields */
-			'delay' => 					array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'required' => false, 'size' => 128),
-			'description' => 			array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 255),
-		),
-	);
+            /* Lang fields */
+            'delay' => 					array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'required' => true, 'size' => 128),
+            'description' => 			array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 255),
+            'name' => 		        	array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 150),
+            'address' => 	    		array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 255),
+            'phone' => 			        array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 50),
+            'mention' => 	    		array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'size' => 255),
+        ),
+    );
 
 	public function getOrders()
 	{
